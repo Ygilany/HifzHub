cb5f116cdd68ce9e59e0b2bcb133fcdb193ed6cc
Yahya Gilany <yahya.gilany@live.com>
2025-12-20 17:50:03 -0500

    feat(database): export student profiles, goals, and sessions schemas

--- packages/database/src/schema/index.ts
+++ packages/database/src/schema/index.ts
@@ -2,4 +2,7 @@ export * from "./users";
 export * from "./programs";
 export * from "./classes";
 export * from "./relationships";
+export * from "./student-profiles";
+export * from "./goals";
+export * from "./sessions";
 
